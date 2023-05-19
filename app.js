534f3025d189a42470bc40d298a01bec7e62b30f
Margarita Villaseñor <maggie.villasenor@hotmail.com>
2023-05-19 11:19:19 -0700

    ci: creation of integration testing

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ require("dotenv").config();
 app.use(express.json());
 app.use("/projects", ProjectRoutes);
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL);
-        app.listen(process.env.PORT);
-    } catch (err) {
-        console.log("Failed to connect to Mongo", err);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+  } catch (err) {
+    console.log("Failed to connect to Mongo", err);
+  }
 };
 
-connectDB();
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running in ${process.env.PORT}`);
+  connectDB();
+});
+
+module.exports = { app, server };
--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,58 +1,58 @@
 const ProjectService = require("../services/projects");
 
 exports.getProjects = async (req, res) => {
-    try {
-        let projects = await ProjectService.getProjects();
-        res.json({
-            projects: projects,
-        });
-    } catch (err) {
-        console.error("err", err);
-        res.status(500).json({
-            message: "Project were not retrieved",
-        });
-    }
+  try {
+    const projects = await ProjectService.getProjects();
+    res.json({
+      projects: projects,
+    });
+  } catch (err) {
+    console.error("err", err);
+    res.status(500).json({
+      message: "Project were not retrieved",
+    });
+  }
 };
 
-exports.getProjectById =async (req, res) => {
-    try {
-        let project = await ProjectService.getProjectById(req.params.id);
-        res.json({
-            project: project,
-        });
-    } catch (err) {
-        console.error("err", err);
-        res.status(404).json({
-            message: "Project was not found",
-        });
-    }
+exports.getProjectById = async (req, res) => {
+  try {
+    let project = await ProjectService.getProjectById(req.params.id);
+    res.json({
+      project: project,
+    });
+  } catch (err) {
+    console.error("err", err);
+    res.status(404).json({
+      message: "Project was not found",
+    });
+  }
 };
 
 exports.createProject = async (req, res) => {
-    try {
-        let projectSaved = await ProjectService.createProject(req.body)
-        res.status(201).json({
-            message: "Project created",
-            projectSaved: projectSaved
-        })
-    } catch (err) {
-        console.error("err" ,err);
-        res.status(400).json({
-            message: "Was not able to create the project",
-        });
-    }
+  try {
+    let projectSaved = await ProjectService.createProject(req.body);
+    res.status(201).json({
+      message: "Project created",
+      projectSaved: projectSaved,
+    });
+  } catch (err) {
+    console.error("err", err);
+    res.status(400).json({
+      message: "Was not able to create the project",
+    });
+  }
 };
 
 exports.updateProject = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const projectData = req.body;
-    
-        const updatedProject = await ProjectService.updateProject(id, projectData);
-    
-        res.status(200).json(updatedProject);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message: "Internal error"});
-    }
+  try {
+    const { id } = req.params;
+    const projectData = req.body;
+
+    const updatedProject = await ProjectService.updateProject(id, projectData);
+
+    res.status(200).json(updatedProject);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
 };
